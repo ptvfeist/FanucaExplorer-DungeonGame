4648fee138f8168b3f873f3562325a5a56b0de14
Code Cadet <codecadet@195-23-74-113.net.novis.pt>
2022-06-07 17:44:58 +0100

    walls and floor

--- Dungeon/src/org/academiadecodigo/unbitables/components/Floor.java
+++ Dungeon/src/org/academiadecodigo/unbitables/components/Floor.java
@@ -3,6 +3,6 @@ package org.academiadecodigo.unbitables.components;
 public class Floor extends Blocks{
 
     public Floor(int x, int y) {
-        super(x, y, "floorTiles.jpeg");
+        super(x, y, "floor.jpeg");
     }
 }
